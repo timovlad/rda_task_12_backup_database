53988442b9e81a50f6f16e62872736ef8d503edc
timovlad <61474934+timovlad@users.noreply.github.com>
2025-04-11 18:33:42 +0200

    Update task.sh

--- task.sh
+++ task.sh
@@ -1,4 +1,4 @@
-#! /bin/bash
+#!/bin/bash
 
 DB_USER="${DB_USER}"
 DB_PASSWORD="${DB_PASSWORD}"
@@ -11,10 +11,10 @@ BACKUP_DIR="/var/backups/mysql"
 TIMESTAMP=$(date +"%Y%m%d_%H%M%S")
 
 mkdir -p ${BACKUP_DIR}
-mysqldump -u ${DB_USER} -p${DB_PASSWORD} --databases ${SOURCE_DB} > ${BACKUP_DIR}/${SOURCE_DB}_backup_${TIMESTAMP}.sql
+mysqldump -u "${DB_USER}" -p"${DB_PASSWORD}" --databases ${SOURCE_DB} > ${BACKUP_DIR}/${SOURCE_DB}_backup_${TIMESTAMP}.sql
 
-mysql -u ${DB_USER} -p${DB_PASSWORD} ${BACKUP_DB} < ${BACKUP_DIR}/${SOURCE_DB}_backup_${TIMESTAMP}.sql
+mysql -u "${DB_USER}" -p"${DB_PASSWORD}" ${BACKUP_DB} < ${BACKUP_DIR}/${SOURCE_DB}_backup_${TIMESTAMP}.sql
 
-mysqldump -u ${DB_USER} -p${DB_PASSWORD} --no-create-info ${SOURCE_DB} > ${BACKUP_DIR}/${SOURCE_DB}_data_${TIMESTAMP}.sql
+mysqldump -u "${DB_USER}" -p"${DB_PASSWORD}" --no-create-info ${SOURCE_DB} > ${BACKUP_DIR}/${SOURCE_DB}_data_${TIMESTAMP}.sql
 
-mysql -u ${DB_USER} -p${DB_PASSWORD} ${DEV_DB} < ${BACKUP_DIR}/${SOURCE_DB}_data_${TIMESTAMP}.sql
+mysql -u "${DB_USER}" -p"${DB_PASSWORD}" ${DEV_DB} < ${BACKUP_DIR}/${SOURCE_DB}_data_${TIMESTAMP}.sql
